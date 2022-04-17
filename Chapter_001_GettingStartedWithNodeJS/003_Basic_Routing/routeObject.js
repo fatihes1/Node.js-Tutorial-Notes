2a2fe0ca068d74c7afc01b2167e50a205f6e5c7d
Fatih Es <54971670+fatihes1@users.noreply.github.com>
2022-04-18 00:03:59 +0300

    routing with object

---
+++ Chapter_001_GettingStartedWithNodeJS/003_Basic_Routing/routeObject.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+var routes = {
+    "/" : function index (req, res) {
+        res.writeHead(200);
+        res.end("Hello Node.js with Object Routing")
+    },
+    "/foo" : function foo(req, res) {
+        res.writeHead(200);
+        res.end("You are now viewing 'foo' page!");
+    }
+}
+
+http.createServer( function(req, res){
+    if(req.url in routes){
+        return routes[req.url](req, res);
+    }
+    // Else situation
+    res.writeHead(404);
+    res.end(http.STATUS_CODES[404]);
+}).listen(1337);
